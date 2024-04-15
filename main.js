cfcc014a1a0a56976e47186222e7e9693dc2c499
DanArtt <xdogman5@gmail.com>
2024-04-15 17:35:16 -0300

    Mudancas na pagina principal

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import { inicializarCarrinho } from "./menuCarrinho";
 const catalogo = [
   {
       id: 1,
-      nomeProduto: "Whey Protein MaxTitanium 1kg",
+      nomeProduto: "Whey Protein MaxTitanium 900g",
       especificacao: "Sabor Baunilha",
-      precoPromocao: 119.99,
-      precoReal: 89.99,
+      precoPromocao: 89.99,
+      precoReal: 199.99,
       nomeArquivoImagem1: "whey-frente.jpeg",
       quantiVendas: 1298,
       estrela: "estrela.png",
@@ -16,8 +16,8 @@ const catalogo = [
       id: 2,
       nomeProduto: "Blusa Preta MaxTitanium Masculina",
       especificacao: "100% Poliéster",
-      precoPromocao: 129.99,
-      precoReal: 99.99,
+      precoPromocao: 99.99,
+      precoReal: 129.99,
       nomeArquivoImagem1: "blusa-max-masculina.jpeg",
       quantiVendas: 423,
       estrela: "estrela.png",
@@ -27,8 +27,8 @@ const catalogo = [
       id: 3,
       nomeProduto: "Cinturão para Agachamento",
       especificacao: "100% couro",
-      precoPromocao: 149.99,
-      precoReal: 119.99,
+      precoPromocao: 119.99,
+      precoReal: 149.99,
       nomeArquivoImagem1: "cinturao.jpeg",
       quantiVendas: 187,
       estrela: "estrela.png",
@@ -38,8 +38,8 @@ const catalogo = [
       id: 4,
       nomeProduto: "Coqueteleira Preta Dark Lab",
       especificacao: "Tampa e Peneira",
-      precoPromocao: 79.99,
-      precoReal: 49.99,
+      precoPromocao: 49.99,
+      precoReal: 79.99,
       nomeArquivoImagem1: "coqueteleiraDark.jpeg",
       quantiVendas: 745,
       estrela: "estrela.png",
@@ -49,8 +49,8 @@ const catalogo = [
       id: 5,
       nomeProduto: "Coqueteleira Preta Growth",
       especificacao: "Tampa e Peneira",
-      precoPromocao: 39.99,
-      precoReal: 19.99,
+      precoPromocao: 19.99,
+      precoReal: 49.99,
       nomeArquivoImagem1: "coqueteleiraGrowth.png",
       quantiVendas: 457,
       estrela: "estrela.png",
@@ -58,10 +58,10 @@ const catalogo = [
   },
   {
       id: 6,
-      nomeProduto: "Creatina Probiotica Monohidratada",
-      especificacao: "300g sem sabor",
-      precoPromocao: 79.99,
-      precoReal: 49.99,
+      nomeProduto: "Creatina Probiotica 300g",
+      especificacao: "Monohidratada",
+      precoPromocao: 49.99,
+      precoReal: 79.99,
       nomeArquivoImagem1: "creatina-frente.jpeg",
       quantiVendas: 1232,
       estrela: "estrela.png",
@@ -71,8 +71,8 @@ const catalogo = [
       id: 7,
       nomeProduto: "Cropped Preto Growth Feminino",
       especificacao: "100% Poliéster",
-      precoPromocao: 99.99,
-      precoReal: 69.99,
+      precoPromocao: 69.99,
+      precoReal: 99.99,
       nomeArquivoImagem1: "cropped-growth-feminino.jpeg",
       quantiVendas: 659,
       estrela: "estrela.png",
@@ -82,8 +82,8 @@ const catalogo = [
       id: 8,
       nomeProduto: "Faixa de Joelho",
       especificacao: "Faixa Elástica com 2 Metros",
-      precoPromocao: 119.99,
-      precoReal: 99.99,
+      precoPromocao: 99.99,
+      precoReal: 119.99,
       nomeArquivoImagem1: "faixa-de-joelho.jpeg",
       quantiVendas: 379,
       estrela: "estrela.png",
@@ -93,8 +93,8 @@ const catalogo = [
       id: 9,
       nomeProduto: "Hipercalórico Growth 3kg",
       especificacao: "Big Mass Ultra Premium Blend",
-      precoPromocao: 149.99,
-      precoReal: 119.99,
+      precoPromocao: 119.99,
+      precoReal: 149.99,
       nomeArquivoImagem1: "hipercalorico-frente.jpeg",
       quantiVendas: 1289,
       estrela: "estrela.png",
@@ -104,8 +104,8 @@ const catalogo = [
       id: 10,
       nomeProduto: "Pré Treino MaxTitauim Hórus 150g",
       especificacao: "Sabor Frutas Vermelhas",
-      precoPromocao: 89.99,
-      precoReal: 69.99,
+      precoPromocao: 69.99,
+      precoReal: 89.99,
       nomeArquivoImagem1: "pretreino-frente.jpeg",
       quantiVendas: 781,
       estrela: "estrela.png",
@@ -115,8 +115,8 @@ const catalogo = [
       id: 11,
       nomeProduto: "Regata Preta Growth Masculina",
       especificacao: "100% Algodão",
-      precoPromocao: 89.99,
-      precoReal: 59.99,
+      precoPromocao: 59.99,
+      precoReal: 89.99,
       nomeArquivoImagem1: "regata-growth-masculina.jpeg",
       quantiVendas: 459,
       estrela: "estrela.png",
@@ -126,8 +126,8 @@ const catalogo = [
       id: 12,
       nomeProduto: "Short Preto Growth Feminino",
       especificacao: "100% Algodão",
-      precoPromocao: 129.99,
-      precoReal: 99.99,
+      precoPromocao: 99.99,
+      precoReal: 129.99,
       nomeArquivoImagem1: "short-growth-feminino.jpeg",
       quantiVendas: 379,
       estrela: "estrela.png",
@@ -137,8 +137,8 @@ const catalogo = [
       id: 13,
       nomeProduto: "Short Preto Growth Masculino",
       especificacao: "100% Algodão",
-      precoPromocao: 119.99,
-      precoReal: 89.99,
+      precoPromocao: 89.99,
+      precoReal: 119.99,
       nomeArquivoImagem1: "short-growth-masculino.jpeg",
       quantiVendas: 587,
       estrela: "estrela.png",
@@ -148,8 +148,8 @@ const catalogo = [
       id: 14,
       nomeProduto: "Short Branco Growth Masculino",
       especificacao: "100% Algodão",
-      precoPromocao: 129.99,
-      precoReal: 99.99,
+      precoPromocao: 99.99,
+      precoReal: 129.99,
       nomeArquivoImagem1: "short-growth-masculino2.jpeg",
       quantiVendas: 678,
       estrela: "estrela.png",
@@ -159,13 +159,92 @@ const catalogo = [
       id: 15,
       nomeProduto: "Par Strap",
       especificacao: "100% Algodão",
-      precoPromocao: 59.99,
-      precoReal: 39.99,
+      precoPromocao: 39.99,
+      precoReal: 59.99,
       nomeArquivoImagem1: "strap.jpeg",
       quantiVendas: 290,
       estrela: "estrela.png",
       nota: 4.7,
   },
+  {
+    id: 16,
+    nomeProduto: "Camiseta Branca Growth Masculina",
+    especificacao: "100% Poliéster",
+    precoPromocao: 69.99,
+    precoReal: 79.99,
+    nomeArquivoImagem1: "camisa-growth-branca.jpeg",
+    quantiVendas: 430,
+    estrela: "estrela.png",
+    nota: 4.5,
+},
+{
+    id: 17,
+    nomeProduto: "Camiseta preta Growth Masculina",
+    especificacao: "100% Poliéster",
+    precoPromocao: 59.99,
+    precoReal: 69.99,
+    nomeArquivoImagem1: "camisa-growth.jpeg",
+    quantiVendas: 423,
+    estrela: "estrela.png",
+    nota: 4.7,
+},
+{
+    id: 17,
+    nomeProduto: "Creatina Growth 250g",
+    especificacao: "Monohidratada",
+    precoPromocao: 89.99,
+    precoReal: 109.99,
+    nomeArquivoImagem1: "creatina-growth.jpeg",
+    quantiVendas: 689,
+    estrela: "estrela.png",
+    nota: 4.9,
+},
+{
+    id: 17,
+    nomeProduto: "Pré Treino Raze 300g",
+    especificacao: "Sabor Laranja",
+    precoPromocao: 89.99,
+    precoReal: 189.99,
+    nomeArquivoImagem1: "pre-treino-growth.png",
+    quantiVendas: 689,
+    estrela: "estrela.png",
+    nota: 4.9,
+},
+
+{
+    id: 17,
+    nomeProduto: "Short Feminino Growth Azul",
+    especificacao: "100% Algodão",
+    precoPromocao: 99.99,
+    precoReal: 119.99,
+    nomeArquivoImagem1: "short-feminino-growth.png",
+    quantiVendas: 543,
+    estrela: "estrela.png",
+    nota: 4.5,
+},
+{
+    id: 17,
+    nomeProduto: "Top Feminino Growth",
+    especificacao: "100% Poliéster",
+    precoPromocao: 89.99,
+    precoReal: 109.99,
+    nomeArquivoImagem1: "top-growth.png",
+    quantiVendas: 354,
+    estrela: "estrela.png",
+    nota: 4.7,
+},
+{
+    id: 18,
+    nomeProduto: "Whey Protein Growth 1kg",
+    especificacao: "Sabor Natural",
+    precoPromocao: 99.99,
+    precoReal: 109.99,
+    nomeArquivoImagem1: "Whey-protein-growth.png",
+    quantiVendas: 1434,
+    estrela: "estrela.png",
+    nota: 4.9,
+},
+
 
 
 
@@ -194,6 +273,26 @@ for (const produtoCatalogo of catalogo) {
 
   document.getElementById('container-produto').innerHTML += cartaoProduto;
 
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("carousel-slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Troca de slide a cada 3 segundos (3000 milissegundos)
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
 inicializarCarrinho()
